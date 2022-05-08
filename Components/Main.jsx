c5f17a057b1c1a9cfcbce41ecca29d55c927a6e4
Yasen Martn <blow-blow@outlook.com>
2022-05-08 21:37:47 +0100

    mobile contact

--- Components/Main.jsx
+++ Components/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from 'next/image';
+import Link from 'next/link';
 import { ImLinkedin2, ImTwitter, ImWhatsapp, ImInstagram, ImFacebook2 } from "react-icons/im";
 
 const Main = () => {
@@ -11,13 +11,17 @@ const Main = () => {
         <h1 className='header'>I&apos;m a Front-end Web Developer</h1>
         <span className='text'>Welcome to my official portfolio website where i put all my projects, technologies i&apos;m using and more.</span>
         <button className='btn1'>Download CV</button>
-        <button className='btn2'>Hire Me</button>
+        <Link href="/contact" >
+          <button className="btn2">
+            Hire Me
+          </button>
+        </Link>
         <div className="socialsContainer">
-          <ImFacebook2 className='icon icon1'/>
-          <ImInstagram className="icon icon2"/>
-          <ImTwitter className='icon icon3'/>
+          <ImFacebook2 className='icon icon1' />
+          <ImInstagram className="icon icon2" />
+          <ImTwitter className='icon icon3' />
           <ImWhatsapp className="icon icon4" />
-          <ImLinkedin2 className='icon icon5'/>
+          <ImLinkedin2 className='icon icon5' />
         </div>
       </div>
 
